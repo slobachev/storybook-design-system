2b10e01a31da36c8777751698a215228d0cc9583
slobachev <sergeylobachev@outlook.com>
2024-03-16 21:27:00 +0200

    add test workflow

--- src/Button/Button.stories.jsx
+++ src/Button/Button.stories.jsx
@@ -6,6 +6,8 @@ import { Icon } from '../Icon/Icon';
 
 import { StoryLinkWrapper } from '../LinkWrapper';
 
+import { expect, userEvent, within } from '@storybook/test';
+
 const CustomButton = styled.button`
   border: 1px solid green;
   background: lightgreen;
@@ -326,5 +328,26 @@ export const AnchorWrapper = {
       </Button>
     </div>
   ),
+};
+
+/*
+ * New story using the play function.
+ */
+export const WithInteractions = {
+  args: {
+    appearance: 'primary',
+    href: 'http://storybook.js.org',
+    ButtonWrapper: StoryLinkWrapper,
+    children: 'Button',
+  },
+  play: async ({ canvasElement }) => {
+    // Assigns canvas to the component root element
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('link'));
+    expect(canvas.getByRole('link')).toHaveAttribute(
+      'href',
+      'http://storybook.js.org',
+    );
+  },
 };
 
